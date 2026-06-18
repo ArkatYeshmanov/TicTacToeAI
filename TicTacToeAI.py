fcfd623e198dcd697300b0c82aecd0148e72ce79
ArkatYeshmanov <arkateshmanov@gmail.com>
2026-06-18 13:00:49 +0500

    Fix typo in comment for player input function

--- TicTacToeAI.py
+++ TicTacToeAI.py
@@ -15,7 +15,7 @@ def printBoard(board):
     print(board[0] + '|' + board[1] + '|' + board[2])
     print(board[3] + '|' + board[4] + '|' + board[5])
     print(board[6] + '|' + board[7] + '|' + board[8])
-#Recieving a player's input
+#Recieving player's input
 def playerInput(board):
     #Checking if player's input is valid
     try:
